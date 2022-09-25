e9646bd06fb34d46251931981ec06b1f58d744a3
NoTitec <zkxkr0@gmail.com>
2022-09-25 23:24:09 +0900

    Polymorphism2

--- Polymorphism/Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism/Polymorphism.cpp
@@ -14,12 +14,28 @@ using namespace std;
 
 //동적 바인딩을 원하면 가상함수를 사용해야 한다.!!!!
 
+//virtual table 생성 정적보다 아주약간 느려짐
+
+// 순수 가상 함수: 구현없이 인터페이스만 전달
 class Player
 {
+public:
+    Player()
+    {
+        _hp = 100;
+    }
 public:
     void Move() { cout << "Move Player!" << endl; }
     //void Move(int a) { cout << "Mover Player int" << endl; }
     virtual void VMove() { cout << "Move Player!" << endl; }//부모에서 virtual지정하는순간 그 하위 모든 클래스에서도 virtual이다
+    virtual void VDie() { cout << "VDie Player" << endl; }
+
+
+    //순수 가상 함수(하위 클래스는 무조건 이 함수 재정의해서 써야함
+    //이함수가 하나라도 있으면 해당 클래스는 추상클래스가됨
+    //추상클래스는 객체화 불가능
+    virtual void VAttack() = 0;
+
 public:
     int _hp;
 };
@@ -27,8 +43,15 @@ public:
 class Knight :public Player
 {
 public:
+    Knight()
+    {
+        _stamina = 100;
+    }
+public:
     void Move() { cout << "Move Knight!" << endl; }
     virtual void VMove() { cout << "Move Knight!" << endl; }
+    virtual void VDie() { cout << "VDie Knight" << endl; }
+    virtual void VAttack() { cout << " Attack" << endl; }
 public:
     int _stamina;
 
@@ -36,32 +59,31 @@ public:
 class Mage : public Player
 {
 
+
+public:
+    int _mp;
 };
 
+// is a 관계에서 부모 포인터를 받아야하는위치에 자식 포인터넣어도 틀린말은 아님
 void MovePlayer(Player* player) {// 정적 바인딩시 이함수 하나라 자식 클래스도 이거로 호출
+    //player 포인터 타고가면 시작주소는 virtual table 주소
+    //이를 타고가서 어떤 virtual 함수 호출해야할지 알아냄
+    // 어떤 객체인지 virtual 주소 알아내는가?-> 생성자에서 지정함
     player->VMove();
+    player->VDie();
 }
 
 int main()
 {
-    Player p;
+    //Player p;
     //p.Move();
     
     Knight k;
     //k.Move();
-    MovePlayer(&p);
+    //MovePlayer(&p);
     //MovePlayer(&k);// 플레이어는 기사다는 거짓이라 오류뜸
     //MoveKnight(&k);
     MovePlayer(&k);// 부모 클래스의 함수를 호출함
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
